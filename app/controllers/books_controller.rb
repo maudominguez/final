bfe292cee94813a71ba7694627e57988ca3df1a3
Mauricio Dominguez <mauricio.dar@gmail.com>
2016-05-15 19:44:05 -0500

    Added new and create actions for Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,31 @@ class BooksController < ApplicationController
   end
 
   def show
-  @book = Book.find_by(id: params[:id])
-  if @book == nil
-    redirect_to books_url
+    @book = Book.find_by(id: params[:id])
+    if @book == nil
+      redirect_to books_url
+    end
+    cookies["book_id"] = @book.id
   end
-  cookies["book_id"] = @book.id
-end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    @book.title = params[:book][:title]
+    @book.description = params[:book][:description]
+    @book.image_url = params[:book][:image_url]
+    @book.genre_id = params[:book][:genre_id]
+    @book.year = params[:book][:year]
+    if @book.save
+      redirect_to books_url, notice: "Book added! Thanks!"
+    else
+      # redirect_to new_book_url, notice: "Something went wrong!"
+      render 'new'
+    end
+  end
+
 
 end
