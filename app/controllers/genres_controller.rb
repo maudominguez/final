92d748a079b447204376cfde31180378195dcd9d
Mauricio Dominguez <mauricio.dar@gmail.com>
2016-05-25 15:55:24 -0500

    Added genres functionality

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -9,15 +9,18 @@ class GenresController < ApplicationController
   end
 
   def new
-
+    @genre = Genre.new
   end
 
   def create
     genre = Genre.new
-    genre.name = params[:name]
-    genre.photo_url = params[:photo_url]
-    genre.save
-    redirect_to genres_url
+    genre.name = params[:genre][:name]
+    genre.description = params[:genre][:description]
+    if genre.save
+      redirect_to genres_url, notice: "Created genre: #{genre.name}"
+    else
+      redirect_to new_genre_url, notice: "Something went wrong!"
+    end
   end
 
   def edit
@@ -25,17 +28,20 @@ class GenresController < ApplicationController
   end
 
   def update
-    genre = Genre.find_by(id: params[:id])
-    genre.name = params[:name]
-    genre.photo_url = params[:photo_url]
-    genre.save
-    redirect_to genres_url
+    @genre = Genre.find_by(id: params[:id])
+    @genre.name = params[:genre][:name]
+    @genre.description = params[:genre][:description]
+    if @genre.save
+      redirect_to genre_url(@genre), notice: "Genre updated"
+    else
+      redirect_to @genre, notice: 'Could not update genre'
+    end
   end
 
   def destroy
     genre = Genre.find_by(id: params[:id])
     genre.destroy
-    redirect_to genres_url
+    redirect_to genres_url, notice: 'Genre destroyed'
   end
 
 end
